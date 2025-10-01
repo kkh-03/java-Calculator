b6f3ca5331452685fd67bc9d12949fbe7586f479
kkh <kkh99480@naver.com>
2025-10-01 10:47:51 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,6 @@ public class Main {
         calc.add();
         calc.displayResult( "+");
 
-        Calculator calc_sub = new Calculator();
-        calc_sub.setNumbers(10,2);
-        calc_sub.subtract();
-        calc_sub.displayResult( "-");
-
-        Calculator calc_mul = new Calculator();
-        calc_mul.setNumbers(10,2);
-        calc_mul.multiply();
-        calc_mul.displayResult( "*");
-
-      Calculator calc_div = new Calculator();
-      calc_div.setNumbers(10,2);
-      calc_div.divide();
-      calc_div.displayResult( "/");
-
-
     }
 
 }
