aa7567d8d84058df4748bcb6bd36fcaa834cb7b0
kkh <kkh99480@naver.com>
2025-10-02 13:38:44 +0900

    AdvancedCalculator 주석 추가 완료

--- src/AdvancedCalculator.java
+++ src/AdvancedCalculator.java
@@ -1,11 +1,15 @@
 public class AdvancedCalculator extends Calculator {
+   //계산기 v2.0 곱하기 기능 개발 완료
+    // https://github.com/kkh-03/java-Calculator/issues/2
+
     @Override
     public int multiply()
     {
         result = num1*num2;
         return result;
     }
-
+// 나눗셈 기능 개발 완료
+    //https://github.com/kkh-03/java-Calculator/issues/3
     public int divide()
     {
         result = num1/num2;
