9191f0b0bb36f286b26c180a99bcef8d2f404f33
kkh <kkh99480@naver.com>
2025-10-22 04:45:25 +0900

    .

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,10 @@ public class Main {
         calc.add();
         calc.displayResult( "+");
 
-<<<<<<< HEAD
-
-
-
-=======
         AdvancedCalculator calc_mul = new AdvancedCalculator();
         calc_mul.setNumbers(10,2);
         calc_mul.multiply();
         calc_mul.displayResult( "*");
->>>>>>> dev/Github-Issue-1
     }
 
 }
